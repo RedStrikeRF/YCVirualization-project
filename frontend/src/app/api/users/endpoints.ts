23e30babb798aa3fca431864965ec1b243200b7c
Kdzefuf <115563854+Kdzefuf@users.noreply.github.com>
2025-05-29 01:59:28 +0500

    cd

--- frontend/src/app/api/users/endpoints.ts
+++ frontend/src/app/api/users/endpoints.ts
@@ -1,7 +1,7 @@
 export const USERS_BASE = '/users';
 
 export const endpoints = {
-  register: `${USERS_BASE}/`,
+  register: `${USERS_BASE}/register`,
   login: `${USERS_BASE}/login`,
   getUsers: `${USERS_BASE}/`,
   userByNickname: (nickname: string) => `${USERS_BASE}/${nickname}`,
